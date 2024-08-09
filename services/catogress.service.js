415225292b305b36e6dce25c28dee03da582ecc2
mohamedadl52 <65096134+mohamedadl52@users.noreply.github.com>
2024-08-08 17:18:37 -0700

    jhghjvbh

--- services/catogress.service.js
+++ services/catogress.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8081/api/catogress';
-// const API_URL = 'https://shop-le2d.onrender.com/api/catogress';
+// const API_URL = 'http://localhost:8081/api/catogress';
+const API_URL = 'https://shop-le2d.onrender.com/api/catogress';
 
 class CatogressService {
   add(product) {
